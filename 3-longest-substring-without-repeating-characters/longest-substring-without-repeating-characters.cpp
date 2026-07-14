235b506f4aa8d2901bba68a406ce308f6f7fd4aa
Anish-72 <ansarianish7788@gmail.com>
2026-07-14 12:35:39 +0530

    Time: 8 ms (56.26%) | Memory: 12 MB (45.28%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,28 +1,21 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        unordered_map<char, int> udm;
+        unordered_map<int,int> udm;
+        int left = 0;
         int maxCount = 0;
-        int left = 0; // Left pointer for our sliding window
 
-        for(int right = 0; right < s.length(); right++) {
+        for(int right = 0; right<s.length(); right++){
             char ch = s[right];
             
-            // If we have seen the character and it is inside our current window
-            if(udm.count(ch) && udm[ch] >= left) {
-                // Shrink the window by moving the left pointer 
-                // just past the previous occurrence of this character
+            if(udm.count(ch) && udm[ch]>=left) {
+
                 left = udm[ch] + 1;
             }
-            
-            // Update the latest index of the character
+
             udm[ch] = right;
-            
-            // Calculate max length for the current window
             maxCount = max(maxCount, right - left + 1);
         }
-        
         return maxCount;
     }
-
 };
